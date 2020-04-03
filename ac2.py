ff36ef71a32d497442e2aaafc9dc72c6759535f9
Elton Martins <elton.martins@live.com>
2020-04-02 21:20:03 -0300

    ac2

--- ac2.py
+++ ac2.py
@@ -29,7 +29,6 @@ def nao_entre_em_panico():
         b += 10
     return primos1
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
