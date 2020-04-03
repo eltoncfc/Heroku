90f24132edba9437a24dc6491a9b2fd89f20244e
Elton Martins <elton.martins@live.com>
2020-04-02 21:15:36 -0300

    ac2

--- ac2.py
+++ ac2.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, jsonify, request
 from math import sqrt
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -28,8 +29,7 @@ def nao_entre_em_panico():
         b += 10
     return primos1
 
-
-if name == "main":
+if __name__ == "__main__"
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
